bfffb3b185986094fea3d829ed6948be191500d2
rabbitt <rabbitt1991x@gmail.com>
2020-06-09 22:04:57 +0430

    game work now !

--- XOgame/index.js
+++ XOgame/index.js
@@ -11,19 +11,20 @@ function marker() {
             console.log("player1")
             playerChar = "X"
             if(this.textContent === "") {
-                this.textContent = playerChar
-                counter +=1
-                checkWin()
-                makePlayer()
+                this.textContent = playerChar;
+                makePlayer();
+                checkWin();
+                counter +=1;
             }
         }else if(player === 2 ){
             console.log("player  2")
             playerChar = "O"
             if(this.textContent === "") {
-                this.textContent = playerChar
-                counter +=1
-                checkWin()
-                makePlayer()
+                this.textContent = playerChar;
+                makePlayer();
+                checkWin();
+                counter +=1;
+
             }
         }
     }
@@ -33,26 +34,76 @@ function makePlayer() {
 }
 
 function resetBord() {
+   counter = 0;
     for (const char of gameBox) {
         char.textContent = ""
     }
+    $("p").eq(-1).text("who win the game ?");
+
 }
 
 // need to fix !
-function checkWin() {
-    var zero  = gameBox.eq(0)
-    var one   = $("td:eq(1)")
-    var two   = $("td:eq(2)")
-    var three = $("td:eq(3)")
-    var four  = $("td:eq(4)")
-    var five  = $("td:eq(5)")
-    var six   = $("td:eq(6)")
-    var seven = $("td:eq(7)")
-    var eight = $("td:eq(8)")
-    if (zero === one){
-        console.log("win")
-    }
+function win() {
 
+    var bordSlot = [gameBox[0].textContent,
+        gameBox[1].textContent,
+        gameBox[2].textContent,
+        gameBox[3].textContent,
+        gameBox[4].textContent,
+        gameBox[5].textContent,
+        gameBox[6].textContent,
+        gameBox[7].textContent,
+        gameBox[8].textContent,
+    ]
+    if (bordSlot[0] === "X" && bordSlot[1] === "X" && bordSlot[2] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    } else if (bordSlot[3] === "X" && bordSlot[5] === "X" && bordSlot[6] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    } else if (bordSlot[7] === "X" && bordSlot[8] === "X" && bordSlot[9] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    } else if (bordSlot[0] === "O" && bordSlot[1] === "O" && bordSlot[2] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    } else if (bordSlot[3] === "O" && bordSlot[4] === "O" && bordSlot[5] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    } else if (bordSlot[6] === "O" && bordSlot[7] === "O" && bordSlot[8] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[2] === "O" && bordSlot[5] === "O" && bordSlot[8] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[1] === "O" && bordSlot[4] === "O" && bordSlot[7] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[0] === "O" && bordSlot[3] === "O" && bordSlot[6] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[2] === "X" && bordSlot[5] === "X" && bordSlot[8] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[1] === "X" && bordSlot[4] ==="X" && bordSlot[7] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[0] === "X" && bordSlot[3] === "X" && bordSlot[6] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[6] === "X" && bordSlot[4] === "X" && bordSlot[2] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[0] === "X" && bordSlot[4] === "X" && bordSlot[8] === "X") {
+        $("p").eq(-1).text("X win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[6] === "O" && bordSlot[4] === "O" && bordSlot[2] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    }else if (bordSlot[0] === "O" && bordSlot[4] === "O" && bordSlot[8] === "O") {
+        $("p").eq(-1).text("O win ! \n if want try more rest!")
+        return true
+    }
 
 }
 
@@ -60,6 +111,17 @@ function bordFull() {
     if(counter <9){
         return false
     }else if (counter === 9){
+        $("p").eq(-1).text("bord is full! \n if want try more rest!");
         return true
+    }
+}
+function checkWin() {
+    winCeckWin = win()
+    borderCheck = bordFull()
+    if (winCeckWin === true){
+        alert("happy win :) \n resat the game! " )
+    }else if(borderCheck === true){
+        alert("bord is full \n resat the game! " )
+
     }
 }
